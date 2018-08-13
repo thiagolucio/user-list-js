18e5bcd738f132ac38d0e38cdefce6d784519ff0
thiagolucio <thiagolucio@gmail.com>
2018-08-13 16:34:32 -0300

    atualizando projeto

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,19 @@
 /* Get the class for first Title of App and input  text value inside */
-// var title = document.querySelector(".first-title"); 
-    var title = document.querySelector(".first-title"); 
+// var title = document.querySelector(".first-title");
+    var title = document.querySelector(".first-title");
     title.textContent = "User List Management";
     // console.log(title.textContent);
-
-//Get Data from User
-
-
-// var user_data = document.querySelector(".tr-user");
-// console.log(user_data);
+    //Get Data from User
+    // var user_data = document.querySelector(".tr-user");
+    // console.log(user_data);
 
 
 
 var users = document.querySelectorAll(".tr-user");
-   
+
 
 for (var i = 0; i < users.length; i++) {
     var user = users[i];
-
-    
 
     //Id from user
     var td_id = user.querySelector(".td-id-user");
@@ -51,14 +46,14 @@ for (var i = 0; i < users.length; i++) {
 
     // var message = "Not be empty";
     // var style =  message.style.color = "#ff0000";
-    
-    if (id == "") {        
+
+    if (id == "") {
         // td_id.textContent = message;
         td_id.textContent = "ID not be empty!";
         // message = style;
         // td_id.textContent = "ID not be empty!";
         // td_id.textContent = message.style.color = "#ff0000";
-        
+
 }
     if (name == "") {
         td_name.textContent = "NAME not be empty!";
@@ -75,8 +70,24 @@ for (var i = 0; i < users.length; i++) {
     if (password == "") {
         td_password.textContent = "PASS not be empty!";
 }
+} //end for
 
+var btnAdd = document.querySelector('#add-user');
+btnAdd.addEventListener("click", function () {
+    event.preventDefault();
+    var form = document.querySelector("#form-add-user");
 
+    var insertFirstName = form.inputFirstName.value;
+    var insertSurname = form.inputSurName.value;
+    var insertAge = form.inputAge.value;
+    var insertEmail = form.inputEmail.value;
+    var insertPassword = form.inputPassword.value;
 
+    console.log(insertFirstName);
+    console.log(insertSurname);
+    console.log(insertAge);
+    console.log(insertEmail);
+    console.log(insertPassword);
 
-}
+})
+//Parei na aula 4 - ultima video aula - Adicionando pacientes na tabela
